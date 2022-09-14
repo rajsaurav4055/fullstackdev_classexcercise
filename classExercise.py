80be684bd4ff9869efc02ab2b86a3908c2d0300e
GurjotSinghArora <94769087+GurjotSinghArora@users.noreply.github.com>
2022-09-13 20:22:29 -0400

    Update classExercise.py

--- classExercise.py
+++ classExercise.py
@@ -1,3 +1,4 @@
+# Saurav's main code
 val_1 = input("Enter first integer value: ")
 val_2 = input("Enter second integer value: ")
 
@@ -8,5 +9,21 @@ exponent = int(val_1) ** int(val_2)
 division = int(val_1) / int(val_2)
 print(subtraction)
 print(summation)
+print(multiplication)
 print(exponent)
 print(division)
+
+# Now adding 3rd variable by Gurjot
+
+val_3= input("Enter third integer value: ")
+subtraction = ((int(val_1) - int(val_2))- int(val_3))
+summation = ((int(val_1) + int(val_2))+int(val_3))
+multiplication = ((int(val_1) * int(val_2))*int(val_3))
+exponent = ((int(val_1) ** int(val_2))**int(val_3))
+division = ((int(val_1) / int(val_2))/int(val_3))
+print(subtraction)
+print(summation)
+print(multiplication)
+print(exponent)
+print(division)
+
